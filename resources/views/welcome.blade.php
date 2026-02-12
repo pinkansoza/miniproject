f88779179d040321a50de80973062ab6ee64d001
Pinkan Sofia Zahra <zahrapinkan19@gmail.com>
2026-02-13 00:58:53 +0700

    edit grid galeri

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,8 @@
 
     {{-- Gallery Grid Section --}}
     <div class="bg-white shadow-inner">
-    <div class="max-w-6xl mx-auto px-4 py-12">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div class="max-w-8xl mx-auto px-6 md:px-10 py-16">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-9">
             @foreach($galleries as $item)
                 <div class="bg-white rounded-2xl shadow-xl overflow-hidden border group flex flex-col h-full">
                     {{-- Gambar dengan Efek Hover --}}
